472c140fecc3485bd2a9f73492045b0d2508e4e1
MSathler <mauriciossr@hotmail.com>
2021-05-25 00:07:10 -0300

    adicionado a retirada de outliers e mudanca da leitura do txt

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Leitura_IMU
-Código para leitura do txt, com a adição dos filtros de média móvel e gaussiano.
+Código para leitura do txt, com a retirada e outliers e adição dos filtros de média móvel e gaussiano.
+
 
 
 ### Entrada de dados em um txt separados por "Espaço":
 
-![image](https://user-images.githubusercontent.com/51409770/119228738-c11dc780-baea-11eb-9f99-dec772b72c55.png)
+- txt no formato {tempo variavel_x variavel_y variavel_z}
+
+![image](https://user-images.githubusercontent.com/51409770/119433420-98652000-bcec-11eb-81aa-0461ea87080b.png)
 
 ## Instalação
 
@@ -30,11 +33,11 @@ Para rodar deve-se setar os parametros:
       
  Exemplos:
         
-        $ python3 init.py x 20 50 giro_190Hz_correia.txt
+        $ python3 init.py x 20 50 giro2_carro.txt
         
-        ou
+ou
         
-        $ python3 Desktop/acelerometro/Leitura_IMU/init.py x 20 50 Desktop/acelerometro/Leitura_IMU/giro_190Hz_correia.txt
+        $ python3 Desktop/acelerometro/Leitura_IMU/init.py x 20 50 Desktop/acelerometro/Leitura_IMU/giro2_carro.txt
 
 
 
@@ -42,6 +45,7 @@ Para rodar deve-se setar os parametros:
 
 ## Gráficos
 
-![image](https://user-images.githubusercontent.com/51409770/119229247-66d23600-baed-11eb-8517-f709247f562f.png)
+![image](https://user-images.githubusercontent.com/51409770/119433472-b599ee80-bcec-11eb-8874-638d5f759c48.png)
+
 
 
--- parse_acc.py
+++ parse_acc.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 from scipy.ndimage import gaussian_filter1d
 import time
+from tqdm import tqdm, trange
 
 class parse_data():
     def __init__(self,filter_sigma = 20,filter_m_movel = 200, posicao_corte = 0, file_name = 'acel_370Hz_correia.txt'):
@@ -20,8 +21,9 @@ class parse_data():
         self._csv_file = open(file_name,'r')
         self.filter_sigma = int(filter_sigma)
         self.X,self.Y,self.Z = [],[],[]
+        # self.last_x,self.last_y,self.last_z = 0,0,0
         
-        t = time.clock_gettime(time.CLOCK_MONOTONIC)       
+        #t = time.clock_gettime(time.CLOCK_MONOTONIC)       
         
         
         for row in self._csv_file:
@@ -29,37 +31,70 @@ class parse_data():
             # print(self._row)
             self._row = self._row[0].split('\t')
             # print(self._row)
-
+            
             if float(self._row[0]) <= self._posicao_corte:
-                pass
+                self.last_x,self.last_y,self.last_z = float(self._row[1]),float(self._row[2]),float(self._row[3])#pass 
             else:
-                self._t_experimento.append(float(self._row[3]))
-                self.X.append(float(self._row[0]))
-                self.Y.append(float(self._row[1]))
-                self.Z.append(float(self._row[2]))
-                self._deque_x.append(float(self._row[0]))
-                self._deque_y.append(float(self._row[1]))
-                self._deque_z.append(float(self._row[2]))
-                    
-                if len(self._deque_x) == self.filter_len:
-                    a = sum(self._deque_x)/self.filter_len
-                    b = sum(self._deque_y)/self.filter_len
-                    c = sum(self._deque_z)/self.filter_len
-                    self._filtrado_x.append(sum(self._deque_x)/self.filter_len)
-                    self._filtrado_y.append(sum(self._deque_y)/self.filter_len)
-                    self._filtrado_z.append(sum(self._deque_z)/self.filter_len)
-        # print(self.X)                
+                
+                self.outlier()
+ 
         for w in range(self.filter_len-1):
-            self._filtrado_x.append(a)
-            self._filtrado_y.append(b)
-            self._filtrado_z.append(c)
-        print("tempo " + str(time.clock_gettime(time.CLOCK_MONOTONIC) - t))
+            self._filtrado_x.append(self.a)
+            self._filtrado_y.append(self.b)
+            self._filtrado_z.append(self.c)
+        #print("tempo " + str(time.clock_gettime(time.CLOCK_MONOTONIC) - t))
         # print(self._filtrado)
         self._V_filtrado_x = gaussian_filter1d(self.X ,self.filter_sigma)
         self._V_filtrado_y = gaussian_filter1d(self.Y ,self.filter_sigma)
         self._V_filtrado_z = gaussian_filter1d(self.Z ,self.filter_sigma)
         self._csv_file.close()
+       
+       
+       
+    def outlier(self):
+        self._t_experimento.append(float(self._row[0]))
+        if abs(abs(self.last_x) - abs(float(self._row[1]))) <= 100:
+            self.last_x = float(self._row[1])
+            self.X.append(float(self._row[1]))
+            self._deque_x.append(float(self._row[1]))
+            if len(self._deque_x) == self.filter_len:
+                self.a = sum(self._deque_x)/self.filter_len
+                self._filtrado_x.append(sum(self._deque_x)/self.filter_len)
+        else:
+            self.X.append(self.last_x)
+            self._deque_x.append(self.last_x)
+            if len(self._deque_x) == self.filter_len:
+                self.a = sum(self._deque_x)/self.filter_len
+                self._filtrado_x.append(sum(self._deque_x)/self.filter_len)
         
+        
+        if abs(abs(self.last_y) - abs(float(self._row[2]))) <= 100:
+            self.last_y = float(self._row[2])
+            self.Y.append(float(self._row[2]))
+            self._deque_y.append(float(self._row[2]))
+            if len(self._deque_x) == self.filter_len:
+                self.b = sum(self._deque_y)/self.filter_len
+                self._filtrado_y.append(sum(self._deque_y)/self.filter_len)
+        else:
+            self.Y.append(self.last_y)
+            self._deque_y.append(self.last_y)
+            if len(self._deque_x) == self.filter_len:
+                self.b = sum(self._deque_y)/self.filter_len
+                self._filtrado_y.append(sum(self._deque_y)/self.filter_len)
+                 
+        if abs(abs(self.last_z) - abs(float(self._row[3]))) <= 100:
+            self.last_z = float(self._row[3])
+            self.Z.append(float(self._row[3]))
+            self._deque_z.append(float(self._row[3]))   
+            if len(self._deque_x) == self.filter_len:
+                self.c = sum(self._deque_z)/self.filter_len
+                self._filtrado_z.append(sum(self._deque_z)/self.filter_len)
+        else:
+            self.Z.append(self.last_z)
+            self._deque_z.append(self.last_z)   
+            if len(self._deque_x) == self.filter_len:
+                self.c = sum(self._deque_z)/self.filter_len
+                self._filtrado_z.append(sum(self._deque_z)/self.filter_len)            
         
     def plot(self,eixo):
         if str(eixo).upper() == "X":
@@ -68,11 +103,11 @@ class parse_data():
             plt.plot(self.X)
             plt.ylim([min(self.X),max(self.X)])
             plt.subplot(132)
-            plt.title(f"Filtro Média Móvel Tamanho {self.filter_len}")
+            plt.title(f"Filtro Média Móvel Período {self.filter_len}")
             plt.plot(self._t_experimento,self._filtrado_x, color='r')
             plt.ylim([min(self.X ),max(self.X )])
             plt.subplot(133)
-            plt.title(f"Filtro Gaussiano Tamanho {self.filter_sigma}")
+            plt.title(f"Filtro Gaussiano Sigma = {self.filter_sigma}")
             plt.plot(self._t_experimento,self._V_filtrado_x, color='green')
             plt.ylim([min(self.X ),max(self.X)])
 
@@ -82,11 +117,11 @@ class parse_data():
             plt.plot(self.Y)
             plt.ylim([min(self.Y),max(self.Y)])
             plt.subplot(132)
-            plt.title(f"Filtro Média Móvel Tamanho {self.filter_len}")
+            plt.title(f"Filtro Média Móvel Período {self.filter_len}")
             plt.plot(self._t_experimento,self._filtrado_y, color='r')
             plt.ylim([min(self.Y),max(self.Y)])
             plt.subplot(133)
-            plt.title(f"Filtro Gaussiano Tamanho {self.filter_sigma}")
+            plt.title(f"Filtro Gaussiano Sigma = {self.filter_sigma}")
             plt.plot(self._t_experimento,self._V_filtrado_y, color='green')
             plt.ylim([min(self.Y),max(self.Y)])
 
@@ -96,11 +131,11 @@ class parse_data():
             plt.plot(self.Z)
             plt.ylim([min(self.Z),max(self.Z)])
             plt.subplot(132)
-            plt.title(f"Filtro Média Móvel Tamanho {self.filter_len}")
+            plt.title(f"Filtro Média Móvel Período {self.filter_len}")
             plt.plot(self._t_experimento,self._filtrado_z, color='r')
             plt.ylim([min(self.Z),max(self.Z)])
             plt.subplot(133)
-            plt.title(f"Filtro Gaussiano Tamanho {self.filter_sigma}")
+            plt.title(f"Filtro Gaussiano Sigma = {self.filter_sigma}")
             plt.plot(self._t_experimento,self._V_filtrado_z, color='green')
             plt.ylim([min(self.Z),max(self.Z)])
         
